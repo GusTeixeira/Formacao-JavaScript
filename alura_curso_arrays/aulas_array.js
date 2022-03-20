35066eb66633913fdf7c96d736f096e69d691cde
GusTeixeira <gusteixeira3004@gmail.com>
2022-03-19 22:12:20 -0300

    Mais funçoes de array,matriz e como localizar um elemento especifico dentro do array

--- alura_curso_arrays/aulas_array.js
+++ alura_curso_arrays/aulas_array.js
@@ -20,6 +20,37 @@ media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
 let array1 = [1, 2, 3]
 let array2 = [4, 5, 6]
 
-array1.concat(array2)
+//console.log(array3)
 
-console.log(array1)
+//Filter
+function isBigEnough(value) {
+    return value >= 10;
+  }
+var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+
+//Slice
+
+const nomes = ['Joao', 'Juliena', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline',
+'Fabiana', 'Andre', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 'Renan',
+'Renata', 'Daisy', 'Camilo']
+
+const sala1 = nomes.slice(0, nomes.length/2)
+const sala2 = nomes.slice(nomes.length/2)
+
+//console.log(`Alunos da Sala 1: ${sala1}`)
+//console.log(`Alunos da Sala 2: ${sala2}`)
+
+//Splice
+
+const listaDeChamada = ['Joao', 'Juliena', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme']
+
+listaDeChamada.splice(1,2, 'Rodrigo')
+
+//console.log(`Lista de chamada ${listaDeChamada}`);
+
+const salaDePython = ['Melissa', 'Helena', 'Rodrigo']
+const salaDeJava = ['Ju', 'Leo', 'Raquel']
+const salaUnificada = salaDeJava.concat(salaDePython)
+
+console.log(salaUnificada)
+
